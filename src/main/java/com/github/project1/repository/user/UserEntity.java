6770223189a833709f78a82163a69715e0a28dda
godssun <98688265+godssun@users.noreply.github.com>
2024-07-16 17:31:00 +0900

    [Repository] UserEntity, UserJpaRepository 생성
    UserEntity, UserJpaRepository 생성

---
+++ src/main/java/com/github/project1/repository/user/UserEntity.java
@@ -0,0 +1,37 @@
+package com.github.project1.repository.user;
+
+
+import jakarta.persistence.*;
+import jakarta.persistence.Id;
+import lombok.*;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "user")
+public class UserEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id")
+	private Integer id;
+
+	@Column(name = "email", unique = true, nullable = false)
+	private String email;
+
+	@Column(name = "password", nullable = false)
+	private String password;
+
+	@Column(name = "username", unique = true)
+	private String username;
+
+	@Column(name = "login_token")
+	private String loginToken;
+
+	@Column(name = "is_logged_in")
+	private boolean isLoggedIn;
+}
